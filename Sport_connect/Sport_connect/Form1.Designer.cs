06c5859f50ec3b717c8d46f4d90f2bfddfb37757
anime454 <anime0856047129@gmail.com>
2021-02-15 11:25:35 +0700

    version 0.0.1

---
+++ Sport_connect/Sport_connect/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Sport_connect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 143);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "UP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(66, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(261, 143);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DOWN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(489, 116);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(619, 335);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "a";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 668);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "test0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Sport_connect/Sport_connect/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sport_connect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SerialPort mySerialPort = new SerialPort();
+        public delegate void UpdateTextBoxDelegate(string value);
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            mySerialPort.PortName = "COM6";
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+            mySerialPort.RtsEnable = true;
+
+            disable_botton();
+            Thread thread = new Thread(new ThreadStart(UpdateTextBoxThread));
+            thread.Start();
+            try
+            {
+                mySerialPort.Open();
+                MessageBox.Show("Connect");
+                enable_botton();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error");
+            }
+        }
+        public void UpdateTextBoxThread()
+        {
+        }
+        public void InvokeUpdateTextBox(string loop)
+        {
+            textBox1.Text = loop;
+        }
+
+        public static void enable_botton()
+        {
+            Form1 bt = new Form1();
+            bt.button1.Enabled = false;
+            bt.button2.Enabled = false;
+        }
+
+        public static void disable_botton()
+        {
+            Form1 bt = new Form1();
+            bt.button1.Enabled = false;
+            bt.button2.Enabled = false;
+        }
+
+        public void DataReceivedHandler(
+                        object sender,
+                        SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadLine();
+            Invoke(new UpdateTextBoxDelegate(InvokeUpdateTextBox), indata);
+            Console.WriteLine(indata);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            byte[] a = BitConverter.GetBytes(15);
+            mySerialPort.Write(a , 0, 1);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+           mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+           byte[] a = BitConverter.GetBytes(-14);
+           mySerialPort.Write(a , 0, 1);
+        }
+
+   
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
